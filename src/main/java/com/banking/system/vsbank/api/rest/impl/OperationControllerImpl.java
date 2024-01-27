e6bd48609ab6051d5c517cf3238154a05d651189
saifallah.ksibi <saif.ksibi@gmail.com>
2024-01-27 14:20:37 +0100

    [FEAT-US-3] use history service in appropriate controller

--- src/main/java/com/banking/system/vsbank/api/rest/impl/OperationControllerImpl.java
+++ src/main/java/com/banking/system/vsbank/api/rest/impl/OperationControllerImpl.java
@@ -2,6 +2,7 @@ package com.banking.system.vsbank.api.rest.impl;
 
 import com.banking.system.vsbank.api.entity.Transaction;
 import com.banking.system.vsbank.api.rest.OperationController;
+import com.banking.system.vsbank.api.service.OperationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,11 +14,14 @@ import java.util.List;
 @Component
 public class OperationControllerImpl implements OperationController {
 
+    @Autowired
+    OperationService operationService;
+
 
     @Override
     public ResponseEntity<List<Transaction>> getTransactionHistory(int id) {
-
-        List<Transaction> transactions = new ArrayList<>();
-        return new ResponseEntity<List<Transaction>>(transactions, HttpStatus.OK);
+        List<Transaction> transactionsDTOS = operationService.getTransactionHistory(id);
+        return  new ResponseEntity<List<Transaction>>(transactionsDTOS, HttpStatus.OK);
     }
+
 }
