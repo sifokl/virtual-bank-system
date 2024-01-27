586783bad51d738d23089c3dee2d13b762f7388a
saifallah.ksibi <saif.ksibi@gmail.com>
2024-01-27 13:05:49 +0100

    [FEAT-US-1] set up project

---
+++ src/main/java/com/banking/system/vsbank/VsbankApplication.java
@@ -0,0 +1,13 @@
+package com.banking.system.vsbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VsbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VsbankApplication.class, args);
+	}
+
+}
